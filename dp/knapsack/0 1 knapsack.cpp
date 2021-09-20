a96fc8490ef1130b44261707e8d3f569e36ce423
deepuyadav004 <77964981+deepuyadav004@users.noreply.github.com>
2021-09-20 21:03:05 +0530

    Create 0 1 knapsack.cpp

---
+++ dp/knapsack/0 1 knapsack.cpp
@@ -0,0 +1,124 @@
+/* 
+You are given weights and values of N items, put these items in a knapsack of capacity W to get the maximum total value in the knapsack. Note that we have only one quantity of each item.
+In other words, given two integer arrays val[0..N-1] and wt[0..N-1] which represent values and weights associated with N items respectively. Also given an integer W which represents knapsack capacity, find out the maximum value subset of val[] such that sum of the weights of this subset is smaller than or equal to W. You cannot break an item, either pick the complete item or don’t pick it (0-1 property).
+*/
+
+// Recursive solution
+ int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       // base case
+       if(n==0 || W==0){
+           return 0;
+       }
+       
+       if(wt[n-1]>W){
+           return knapSack(W,wt,val,n-1);
+       }else{
+           return max(val[n-1]+knapSack(W-wt[n-1],wt,val,n-1), knapSack(W,wt,val,n-1));
+       }
+    }
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// memoization approach
+    
+int knapSackRec(int W, int wt[],
+                int val[], int i,
+                int** dp)
+{
+    // base condition
+    if (i < 0)
+        return 0;
+    if (dp[i][W] != -1)
+        return dp[i][W];
+ 
+    if (wt[i] > W) {
+ 
+        // Store the value of function call
+        // stack in table before return
+        dp[i][W] = knapSackRec(W, wt,
+                               val, i - 1,
+                               dp);
+        return dp[i][W];
+    }
+    else {
+        // Store value in a table before return
+        dp[i][W] = max(val[i]
+                      + knapSackRec(W - wt[i],
+                                   wt, val,
+                                   i - 1, dp),
+                       knapSackRec(W, wt, val,
+                                   i - 1, dp));
+ 
+        // Return value of table after storing
+        return dp[i][W];
+    }
+}
+ 
+int knapSack(int W, int wt[], int val[], int n)
+{
+    // double pointer to declare the
+    // table dynamically
+    int** dp;
+    dp = new int*[n];
+ 
+    // loop to create the table dynamically
+    for (int i = 0; i < n; i++)
+        dp[i] = new int[W + 1];
+ 
+    // loop to initially filled the
+    // table with -1
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < W + 1; j++)
+            dp[i][j] = -1;
+    return knapSackRec(W, wt, val, n - 1, dp);
+}
+
+// bottom up approach
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// A utility function that returns
+// maximum of two integers
+int max(int a, int b)
+{
+    return (a > b) ? a : b;
+}
+ 
+// Returns the maximum value that
+// can be put in a knapsack of capacity W
+int knapSack(int W, int wt[], int val[], int n)
+{
+    int i, w;
+      vector<vector<int>> K(n + 1, vector<int>(W + 1));
+ 
+    // Build table K[][] in bottom up manner
+    for(i = 0; i <= n; i++)
+    {
+        for(w = 0; w <= W; w++)
+        {
+            if (i == 0 || w == 0)
+                K[i][w] = 0;
+            else if (wt[i - 1] <= w)
+                K[i][w] = max(val[i - 1] +
+                                K[i - 1][w - wt[i - 1]],
+                                K[i - 1][w]);
+            else
+                K[i][w] = K[i - 1][w];
+        }
+    }
+    return K[n][W];
+}
+ 
+// Driver Code
+int main()
+{
+    int val[] = { 60, 100, 120 };
+    int wt[] = { 10, 20, 30 };
+    int W = 50;
+    int n = sizeof(val) / sizeof(val[0]);
+     
+    cout << knapSack(W, wt, val, n);
+     
+    return 0;
+}
