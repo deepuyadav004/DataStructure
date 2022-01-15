07898e1fa1ca469bcb19492f61ea89afb0a3e2bc
Deepu Yadav <77964981+deepuyadav004@users.noreply.github.com>
2022-01-15 07:22:11 +0530

    Create check_two_arrays_are_equal_or_not.cpp

---
+++ practice/array/check_two_arrays_are_equal_or_not.cpp
@@ -0,0 +1,37 @@
+Given two arrays A and B of equal size N, the task is to find if given arrays are equal or not. Two arrays are said to be equal if both of them contain same set of elements,
+arrangements (or permutation) of elements may be different though.
+Note : If there are repetitions, then counts of repeated elements must also be same for two array to be equal.
+
+Example 1:
+
+Input:
+N = 5
+A[] = {1,2,5,4,0}
+B[] = {2,4,5,0,1}
+Output: 1
+Explanation: Both the array can be 
+rearranged to {0,1,2,4,5}
+
+
+ bool check(vector<ll> A, vector<ll> B, int N) {
+        //code here
+        unordered_map<int,int>mp;
+        for(int i=0; i<N; i++){
+            mp[A[i]]++;
+        }
+        
+        for(int i=0; i<N; i++){
+            if(mp[B[i]]==0){
+                return false;
+            }
+            mp[B[i]]--;
+        }
+        
+        for(auto i:mp){
+            if(i.second == 1){
+                return false;
+            }
+        }
+        return true;
+    }
+
