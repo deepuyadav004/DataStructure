480eb42ff71c690d893576df00de7fbd6c4f3e78
Deepu Yadav <77964981+deepuyadav004@users.noreply.github.com>
2022-01-19 18:48:26 +0530

    Update kmp_based.cpp

--- practice/string/kmp_based.cpp
+++ practice/string/kmp_based.cpp
@@ -76,3 +76,24 @@ left-rotating s1 by 5 units.
   }
   return true;
 }
+
+
+
+// ------------------------------------------------------------------------------- simple sol -----------------------------------------------------------
+
+ bool areRotations(string s1,string s2)
+    {
+        if(s1.length()!=s2.length())
+            return false;
+        else
+        {
+            //we concatenate first string to itself and check if other 
+            //string occurs in it as substring. If yes, then it 
+            //is rotated version and we return true else false.
+            string concated=s1+s1;
+            if(concated.find(s2)!=string::npos)
+                return true;
+            else
+                return false;
+        }
+    }
