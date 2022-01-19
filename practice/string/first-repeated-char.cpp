f384d9422f60fd6c5268c5c41eb90a8d7605a11f
Deepu Yadav <77964981+deepuyadav004@users.noreply.github.com>
2022-01-20 04:03:40 +0530

    Create first-repeated-char.cpp

---
+++ practice/string/first-repeated-char.cpp
@@ -0,0 +1,11 @@
+Given a string S. The task is to find the first repeated character in it. We need to find the character that occurs more than once and whose index of second occurrence is smallest. S contains only lowercase letters.
+
+ 
+
+Example 1:
+
+Input: S="geeksforgeeks"
+Output: e
+Explanation: 'e' repeats at third position.
+  
+  https://practice.geeksforgeeks.org/problems/find-first-repeated-character4108/1/?category[]=Strings&category[]=Strings&difficulty[]=-1&page=4&query=category[]Stringsdifficulty[]-1page4category[]Strings
