1a51bc6a627840f53d25aee0176fb4150a756837
Deepu Yadav <77964981+deepuyadav004@users.noreply.github.com>
2022-01-31 08:58:22 +0530

    Create subarray_sum_equals_k.cpp

---
+++ practice/greedy/subarray_sum_equals_k.cpp
@@ -0,0 +1,44 @@
+leetcode
+
+Given an array of integers nums and an integer k, return the total number of continuous subarrays whose sum equals to k.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+  
+  
+  The idea behind this approach is as follows: If the cumulative sum(represented by sum[i]sum[i] for sum up to i^{th}i 
+th
+  index) up to two indices is the same, the sum of the elements lying in between those indices is zero. Extending the same thought further, if the cumulative sum up to two indices, say ii and jj is at a difference of kk i.e. if sum[i] - sum[j] = ksum[i]−sum[j]=k, the sum of elements lying between indices ii and jj is kk.
+
+Based on these thoughts, we make use of a hashmap mapmap which is used to store the cumulative sum up to all the indices possible along with the number of times the same sum occurs. We store the data in the form: (sum_i, no. of occurrences of sum_i)(sum 
+i
+​
+ ,no.ofoccurrencesofsum 
+i
+​
+ ). We traverse over the array numsnums and keep on finding the cumulative sum. Every time we encounter a new sum, we make a new entry in the hashmap corresponding to that sum. If the same sum occurs again, we increment the count corresponding to that sum in the hashmap. Further, for every sum encountered, we also determine the number of times the sum sum-ksum−k has occurred already, since it will determine the number of times a subarray with sum kk has occurred up to the current index. We increment the countcount by the same amount.
+
+After the complete array has been traversed, the countcount gives the required result.
+  
+  
+    int subarraySum(vector<int>& nums, int k) {
+        int count=0, sum=0;
+        map<int, int>mp;
+        mp[0]=1;
+        for(int i=0; i<nums.size(); i++){
+            sum+=nums[i];
+            if(mp.find(sum-k) != mp.end()){
+                count+=mp[sum-k];
+            }
+            mp[sum]=mp[sum]+1;
+        }
+        return count;
+    }
