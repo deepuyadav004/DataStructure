18bfe001c9c770d737fbf3cf18cf19fa1a37f9cf
Deepu Yadav <77964981+deepuyadav004@users.noreply.github.com>
2022-01-31 07:57:48 +0530

    Create continous_subarray_sum.cpp

---
+++ practice/greedy/continous_subarray_sum.cpp
@@ -0,0 +1,97 @@
+Given an integer array nums and an integer k, return true if nums has a continuous subarray of size at least two whose elements sum up to a multiple of k, or false otherwise.
+
+An integer x is a multiple of k if there exists an integer n such that x = n * k. 0 is always a multiple of k.
+
+ 
+
+Example 1:
+
+Input: nums = [23,2,4,6,7], k = 6
+Output: true
+Explanation: [2, 4] is a continuous subarray of size 2 whose elements sum up to 6.
+  
+  
+  1st
+Naive Approach
+Giving TLE
+Time Complexity = O(N^2), Space Complexity = O(1)
+
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int sum = nums[0];
+        for(int itr = 1; itr < nums.size(); itr++){
+            sum += nums[itr];
+            if(sum % k == 0) return true;
+            
+            int temp = sum;
+            for(int jtr = 0; jtr < itr-1; jtr++){
+                temp -= nums[jtr];
+                if(temp % k == 0) return true;
+            }
+        }
+        return false;
+    }
+};
+2nd
+Efficient Approach
+Using unodered_map
+Time Complexity = O(N), Space Complexity = O(K)
+
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        
+        // taking a map to store the key as a remainder and value as a current index+1
+        unordered_map<int, int> mp;        
+        mp[0] = 0;
+        int sum = 0;
+        
+        for(int itr = 0; itr < nums.size(); itr++){
+            // adding current element into sum
+            sum += nums[itr];
+            
+            // finding the remainder of the sum
+            int sumMod = sum % k;
+            
+            // if remainder is present into map, then check the index of the present remainder into map, and the current index, if the length is more than or equal to 2, it means, we got the subarray of minimum length 2 
+            if(mp.find(sumMod) != mp.end()){
+                if(itr+1 - mp[sumMod] >= 2) return true;
+            }
+            else{   // if remainder is not present into map, then just put that remainder as a key and value as current index+1;
+                mp[sumMod] = itr+1;
+            }
+        }
+        return false;
+    }
+};
+3rd
+Efficient Approach
+Using unodered_set
+Time Complexity = O(N), Space Complexity = O(K)
+
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        
+        // taking a set to store the previous remainder
+        unordered_set<int> st;          
+        int sum = 0, previousRem = 0;
+        
+        for(int itr = 0; itr < nums.size(); itr++){
+            // adding current element into sum
+            sum += nums[itr];
+            
+            // finding the remainder of the sum
+            int sumMod = sum % k;
+            
+            // if count of sumMod in set is more than 0, it means, we got the subarray of minimum length 2 
+            if(st.count(sumMod)) return true;
+            
+            // insert the previousRem
+            st.insert(previousRem);
+            previousRem = sumMod;
+        }
+        return false;
+    }
+};
