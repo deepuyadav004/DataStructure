b6b7c3cecbc013fd5f2df41b1e4d27b12c5174f0
Deepu Yadav <77964981+deepuyadav004@users.noreply.github.com>
2022-03-26 14:50:25 +0530

    Create bipartite_graph.cpp

---
+++ Graphs/bipartite_graph.cpp
@@ -0,0 +1,28 @@
+// using bfs
+
+
+bool checkBipartiteBFS(vector<int> arr[], int n){
+    vector<int>color(n+1, -1);
+    for(int i=1; i<=n; i++){
+        if(color[i]==-1){
+            queue<int>q;
+            q.push(i);
+            color[i]=1;
+
+            while(!q.empty()){
+                int node=q.front();
+                q.pop();
+
+                for(auto it:arr[node]){
+                    if(color[it]==-1){
+                        color[it]=1-color[node];
+                        q.push(it);
+                    }else if(color[it]==color[node]){
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    return true;
+}
