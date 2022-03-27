e7c40288b6d1813a6d5ace9e1a3edf29e9a1fcf2
Deepu Yadav <77964981+deepuyadav004@users.noreply.github.com>
2022-03-27 19:03:39 +0530

    Update bipartite_graph.cpp

--- Graphs/bipartite_graph.cpp
+++ Graphs/bipartite_graph.cpp
@@ -1,4 +1,4 @@
-// using bfs
+// using bfs iterative
 
 
 bool checkBipartiteBFS(vector<int> arr[], int n){
@@ -17,6 +17,36 @@ bool checkBipartiteBFS(vector<int> arr[], int n){
                     if(color[it]==-1){
                         color[it]=1-color[node];
                         q.push(it);
+                    }else if(color[it]==color[node]){
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    return true;
+}
+
+
+// using dfs iterative
+
+
+bool graph_colouring_dfs(vector<int> arr[], int n){
+    vector<int> color(n+1, -1);
+    for(int i=1; i<=n; i++){
+        if(color[i]==-1){
+            color[i]=1;
+            stack<int>st;
+            st.push(i);
+
+            while(!st.empty()){
+                int node=st.top();
+                st.pop();
+
+                for(auto it:arr[node]){
+                    if(color[it]==-1){
+                        color[it]=1-color[node];
+                        st.push(it);
                     }else if(color[it]==color[node]){
                         return false;
                     }
